cbc733256a5bbfb097bfaf1c031a1af70359cb28
wangxiang <wangxiang@yixia.com>
2018-06-08 21:29:58 +0800

    initial commit

--- SHPractice-GCDTimer/ViewController.m
+++ SHPractice-GCDTimer/ViewController.m
@@ -16,14 +16,35 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
 }
 
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)startEvent:(id)sender {
+    [self createTimer];
+}
+
+
+- (IBAction)stopEvent:(id)sender {
+    
+}
+
+
+- (void)createTimer{
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        dispatch_async(dispatch_queue_create("com.test.demo", DISPATCH_QUEUE_CONCURRENT), ^{
+            NSTimer *timer = [NSTimer timerWithTimeInterval:0 target:self selector:@selector(updateTimer) userInfo:nil repeats:YES];
+            [[NSRunLoop currentRunLoop] addTimer:timer forMode:NSRunLoopCommonModes];
+        });
+    });
+}
+
+- (void)updateTimer{
+    NSLog(@"currentThread -------- %@", [NSThread currentThread]);
+}
 
 @end
